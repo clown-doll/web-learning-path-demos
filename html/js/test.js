9d604c2a6b78fbf114620d24af0244df60d6b482
clown-doll <990515207@qq.com>
2016-10-18 14:23:23 +0800

    文档和元数据标签综合案例。

---
+++ html/js/test.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Administrator
+* @Date:   2016-10-17 14:27:19
+* @Last Modified by:   Administrator
+* @Last Modified time: 2016-10-18 13:27:01
+*/
+
+'use strict';
+
+// console.log(2);
